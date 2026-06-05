f298523f581bb45d76976d55c3d456c66b51c966
leehyeok2008-maker <leehyeok2008@gmail.com>
2026-06-05 10:30:05 +0900

    sprite update

--- entities/drone.py
+++ entities/drone.py
@@ -1,4 +1,27 @@
+import math
+import pygame
+from pygame import Vector2, Surface
 from entity import Entity
+from physics.rigidbody2d import RigidBody2D
 
 class Drone(Entity):
-    pass
+    def __init__(
+        self, sprite : Surface, mass : float, moment : float, 
+        position : Vector2 | None = None, velocity : Vector2 | None = None,
+        angle : float = 0.0, angular_velocity : float = 0.0
+    ):
+        super().__init__(
+            sprite=sprite,
+            rigidbody=RigidBody2D(mass, moment, position, velocity, angle, angular_velocity)
+        )
+
+    def update(self, dt) -> list[str]:
+        return []
+    
+    def render(self, screen : Surface) -> None:
+        angle_deg = math.degrees(self.rigidbody.angle)
+        rotated_sprite = pygame.transform.rotate(self.sprite, angle_deg)
+        rect = rotated_sprite.get_rect(center=self.rigidbody.position)
+        screen.blit(rotated_sprite, rect)
+
+        
