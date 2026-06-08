6c99eb008997ec0abeb529372c80a2a10af85337
leehyeok2008-maker <leehyeok2008@gmail.com>
2026-06-08 10:13:06 +0900

    controller update

--- controller/manual_controller.py
+++ controller/manual_controller.py
@@ -1,6 +1,17 @@
+from config import *
 from controller.controller import Controller
 
 class ManualController(Controller):
     def command(self, inputs) -> list[str]:
+        commands = []
         for input in inputs:
-            if input == 
+            if input == KEY_W or input == KEY_UP:
+                commands.append("MOVE_UPWARD")
+            if input == KEY_S or input == KEY_DOWN:
+                commands.append("MOVE_DOWNWARD")
+            if input == KEY_A or input == KEY_LEFT:
+                commands.append("MOVE_LEFT")
+            if input == KEY_D or input == KEY_RIGHT:
+                commands.append("MOVE_RIGHT")
+        return commands            
+                
